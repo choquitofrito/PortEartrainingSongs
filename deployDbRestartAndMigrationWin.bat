a314cccd6f3c35d2c5d914d024f09fdec84c049a
choquitofrito <zyriab@gmail.com>
2023-02-09 10:55:36 +0100

    home controller start

--- deployDbRestartAndMigrationWin.bat
+++ deployDbRestartAndMigrationWin.bat
@@ -6,6 +6,7 @@ php bin/console doctrine:database:drop --force --no-interaction
 php bin/console doctrine:database:create --no-interaction
 php bin/console make:migration --no-interaction
 php bin/console doctrine:migrations:migrate --no-interaction
+@REM php bin/console doctrine:fixtures:load --group=base --no-interaction
 php bin/console doctrine:fixtures:load --group=base --no-interaction
 
 
