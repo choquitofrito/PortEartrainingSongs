9f43cfab18ffe1caebe69374b16696c3db69f30e
choquitofrito <zyriab@gmail.com>
2023-02-09 11:45:28 +0100

    controller Library start

---
+++ src/Controller/LibraryController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Library;
+use App\Repository\LibraryRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class LibraryController extends AbstractController
+{
+
+    private ManagerRegistry $doctrine;
+    public function __construct(ManagerRegistry $doctrine)
+    {
+        $this->doctrine = $doctrine;        
+    }
+    #[Route('/library/show/all', name: 'library_show_all')]
+    public function showAll(LibraryRepository $rep): Response
+    {
+        return $this->render('library/library_show_all.html.twig', ['libraries'=> $rep->findAll()]);
+    }
+
+    #[Route('/library/show/songs/{id}', name: 'library_show_songs')]
+    public function showSongs(Library $library): Response
+    {
+        return $this->render('library/show_songs.html.twig', ['librarySongs'=> $library->getSongs()]);
+    }
+
+
+}
